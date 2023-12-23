b7bb59743a696b7eb91ecf33e907ea7125f7bbbe
tript <triptigupta1998m@gmail.com>
2023-12-23 16:14:35 +0530

    change tempreture from degree C to F

---
+++ src/main/scala/degreeCtoF.scala
@@ -0,0 +1,8 @@
+object degreeCtoF {
+  def main(args: Array[String]): Unit = {
+    var temp_in_C = 80
+    var temp_in_F = (temp_in_C * 9/5) + 32
+    println("Temp in F is " + temp_in_F)
+  }
+
+}
